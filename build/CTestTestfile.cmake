09ce8bae983306707ca763cbe4ac82d085a788d4
Contaro <b.deni.goperty@yandex.ru>
2024-07-25 23:35:01 +0300

    Firts commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/contaro/dev/courses/cpp/lection_1
+# Build directory: /home/contaro/dev/courses/cpp/lection_1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/home/contaro/dev/courses/cpp/lection_1/CMakeLists.txt;74;add_test;/home/contaro/dev/courses/cpp/lection_1/CMakeLists.txt;0;")
