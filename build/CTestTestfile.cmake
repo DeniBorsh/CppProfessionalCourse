842a0464278464b67feae32de0eb67de7eb5ee2c
Contaro <b.deni.goperty@yandex.ru>
2024-07-27 22:54:02 +0300

    6th commit

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,5 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(test_version "test_version")
-set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/home/contaro/dev/courses/cpp/lection_1/CMakeLists.txt;76;add_test;/home/contaro/dev/courses/cpp/lection_1/CMakeLists.txt;0;")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/home/contaro/dev/courses/cpp/lection_1/CMakeLists.txt;75;add_test;/home/contaro/dev/courses/cpp/lection_1/CMakeLists.txt;0;")
 subdirs("_deps/googletest-build")
