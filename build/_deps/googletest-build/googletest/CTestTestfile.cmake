bf939aad3f81654c7d1a9ee1dd77a0f56b298201
Contaro <b.deni.goperty@yandex.ru>
2024-07-27 22:56:14 +0300

    7th commit

--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/contaro/dev/courses/cpp/lection_1/build/_deps/googletest-src/googletest
-# Build directory: /home/contaro/dev/courses/cpp/lection_1/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
